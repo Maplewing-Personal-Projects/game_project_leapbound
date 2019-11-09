c7a9c042bc28b446e75cf194f8ee0afb8d6ce4a2
Maplewing <sinmaplewing@gmail.com>
2019-11-09 17:44:09 +0800

    Implement the camera will follow the player

---
+++ Assets/Scripts/Engine/Library/Unity/VectorExtension.cs
@@ -0,0 +1,11 @@
+﻿using Maplewing.LeapBound.Engine.Data;
+using UnityEngine;
+
+namespace Maplewing.LeapBound.Engine.Unity
+{
+    public static class VectorExtension
+    {
+        public static Vector3 WithZ(this Vector2 vector, float z)
+            => new Vector3(vector.x, vector.y, z);
+    }
+}
--- Assets/Scripts/Scene/GamePlay/GamePlayScene.cs
+++ Assets/Scripts/Scene/GamePlay/GamePlayScene.cs
@@ -7,6 +7,7 @@ namespace Maplewing.LeapBound.Unity.GamePlay
 {
     public class GamePlayScene : MonoBehaviour
     {
+        [SerializeField] private Transform _cameraTransform = null;
         [SerializeField] private Transform _playerTransform = null;
 
         private GamePlayEngine _gamePlayEngine;
@@ -20,7 +21,15 @@ namespace Maplewing.LeapBound.Unity.GamePlay
         {
             _gamePlayEngine = _gamePlayEngine.Update(Time.deltaTime);
 
-            if(_playerTransform != null) _playerTransform.localPosition = _gamePlayEngine.Player.Position.ToVector3();
+            _UpdateViews(_gamePlayEngine);
+        }
+
+        private void _UpdateViews(GamePlayEngine gamePlayEngine)
+        {
+            if (_cameraTransform != null) _cameraTransform.localPosition =
+                    _gamePlayEngine.Player.Position.ToVector2().WithZ(_cameraTransform.localPosition.z);
+
+            if (_playerTransform != null) _playerTransform.localPosition = _gamePlayEngine.Player.Position.ToVector3();
         }
     }
 }
