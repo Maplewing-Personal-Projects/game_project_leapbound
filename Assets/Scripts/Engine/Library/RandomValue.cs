7002ddb607a4dd6a8aae4b6d5230782afb859771
Maplewing <sinmaplewing@gmail.com>
2019-11-10 21:14:34 +0800

    Remove unused comment

--- Assets/Scripts/Engine/Library/RandomValue.cs
+++ Assets/Scripts/Engine/Library/RandomValue.cs
@@ -5,8 +5,7 @@ namespace Maplewing.LeapBound.Engine
     public static class RandomValue
     {
         private static Random _random = new Random();
-
-        // from: https://stackoverflow.com/questions/3365337/best-way-to-generate-a-random-float-in-c-sharp
+        
         public static float GetFloat(float minValue, float maxValue)
             => (float)(_random.NextDouble() * (maxValue - minValue) + minValue);
 
